a2894effbc73ff2f964f524a39c57b0718a61edf
Marije83 <marije.vanhattum@hotmail.nl>
2023-02-20 19:39:53 +0000

    Split experiments from first complete attempt

--- TBAG-code/TODO.py
+++
@@ -1,2 +0,0 @@
-# Find out why validity of moria is not automatically set to False, aka why it is not looping if 
-# Within scenes, have questions as attribute. Split setting up the scene from the execution.
